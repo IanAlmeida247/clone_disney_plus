5c3f41a84039f480c57584c1032d663608334934
IanAlmeida247 <almeidaian318@gmail.com>
2025-03-06 13:40:17 -0300

    criando a seçao de planos

---
+++ src/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`); 
+
+            if (aba) { 
+                escondeTodasAbas();
+                aba.classList.add('shows__list--is-active');
+            }
+
+            removeBotaoAtivo();
+            event.target.classList.add('shows__tabs__button--is-active'); 
+        });
+    }
+});
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
